9215d8ed117e64caef35d0ab7373a04b4bf451db
jasonch15 <101620411+jasonch15@users.noreply.github.com>
2024-11-12 06:28:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-text", {
+        strings: ["歡迎來到我的期中統整專案作品集"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        startDelay: 500,
+        backDelay: 2000,
+        loop: false,
+        showCursor: true,
+        cursorChar: "|",
+    });
+});
